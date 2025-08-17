96c310a70e76e4d104bbfb9ee71acd7748231b9c
MdMahfujHossen.Pr <mdmahfujhossen.pr@gmail.com>
2025-08-17 18:45:41 +0600

    Server Site Compelte Now updated

--- index.js
+++ index.js
@@ -486,7 +486,7 @@ app.put('/api/tasks/:id', async (req, res) => {
   }
 });
 
-// Delete tasks
+// Delete task
 app.delete("/api/tasks/:id", authMiddleware, roleMiddleware("Buyer"), async (req, res) => {
   const taskId = req.params.id;
 
